98cff475f00a5a950d61982738b6d284069003bd
mic <m@micz.it>
2023-12-01 20:43:18 +0100

    prepend mzmo- to all relevant files

--- mzmo-background.js
+++ mzmo-background.js
@@ -1,7 +1,7 @@
 // Register the message display script for all newly opened message tabs.
 messenger.messageDisplayScripts.register({
-    js: [{ file: "js/message-content-script.js" }],
-    css: [{ file: "css/message-content-styles.css" }],
+    js: [{ file: "js/mzmo-message-content-script.js" }],
+    css: [{ file: "css/mzmo-message-content-styles.css" }],
 });
 
 // Inject script and CSS in all already open message tabs.
@@ -12,9 +12,9 @@ let messageTabs = openTabs.filter(
 );
 for (let messageTab of messageTabs) {
     browser.tabs.executeScript(messageTab.id, {
-        file: "js/message-content-script.js"
+        file: "js/mzmo-message-content-script.js"
     })
     browser.tabs.insertCSS(messageTab.id, {
-        file: "css/message-content-styles.css"
+        file: "css/mzmo-message-content-styles.css"
     })
 }
